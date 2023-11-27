38303cb64d49b56c5d2ce9a95d0eddefc72aa2ba
mishukShuvro <shuvro236@gmail.com>
2023-11-27 15:45:28 +0600

    charecters are connect with each other

--- server/index.js
+++ server/index.js
@@ -8,12 +8,38 @@ const io = new Server({
 
 io.listen(3079);
 
+const characters =[];
+
+const generateRandomPosition =() => {
+    return [Math.random() * 3,0, Math.random() * 3];
+};
+
+const generateRandomHexColor = () => {
+    return "#" + Math.floor(Math.random() * 16777215).toString(16);
+};
+
 io.on("connection", (socket) => {
     console.log("user connected");
 
+    characters.push({
+        id: socket.id,
+        position: generateRandomPosition(),
+        shirtColor : generateRandomHexColor(),
+        pantColor : generateRandomHexColor(),
+    })
+
     socket.emit("hello");
+    
+    io.emit("characters",  characters);
 
     socket.on("disconnect", () => {
         console.log("user disconnected");
+
+        characters.splice(
+            characters.findIndex((character)=> character.id == socket.id),
+            1
+        );
+
+        io.emit("characters", characters);
     });
 });
